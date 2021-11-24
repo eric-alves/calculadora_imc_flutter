c0aa6557b4d8457c89b2731b4992c06502231c09
3r1cr312 <eralves01@gmail.com>
2021-11-24 16:24:47 -0300

    first commit

---
+++ android/app/src/main/java/br/com/declatecnologia/calculadora_imc_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.declatecnologia.calculadora_imc_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String _infoText = "Informe seus dados!";
+
+  void _resetFields() {
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe seus dados!";
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      print(imc);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc < 18.6) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 40) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent,
+        actions: [
+          IconButton(
+            onPressed: _resetFields,
+            icon: Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(
+              Icons.person_outline,
+              size: 120.0,
+              color: Colors.pinkAccent,
+            ),
+            TextField(
+              controller: weightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Peso (kg):",
+                labelStyle: TextStyle(color: Colors.pinkAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.pinkAccent,
+                fontSize: 25,
+              ),
+            ),
+            TextField(
+              controller: heightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Altura (cm):",
+                labelStyle: TextStyle(color: Colors.pinkAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.pinkAccent,
+                fontSize: 25,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 10),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: _calculate,
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                  color: Colors.pinkAccent,
+                ),
+              ),
+            ),
+            Text(
+              "$_infoText",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.pinkAccent,
+                fontSize: 25.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
